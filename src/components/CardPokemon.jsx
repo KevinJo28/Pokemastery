241ebe0f27e7b13442c63ff7fca8f0c8bfc49fdb
PC <kevinabarca1224@gmail.com>
2025-01-23 10:23:48 -0600

    Integracion de acceso a los pokemon en home

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import Types from "./Types";
+import { useNavigate } from "react-router-dom";
 
 
 const CardPokemon = ({pokemon}) => {
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-  
+  const navigate = useNavigate();
       
   
   return (
     
-    <div className="pokemon">
+    <div className="pokemon" onClick={()=>{navigate(`/Pokedex/Pokemon/${pokemon.id}`)}}>
     <p className="pokemonid-back">#{pokemon.id}</p>
     <img src={imageUrl} alt={pokemon.name} />
     <div className="pokemonInfo">
